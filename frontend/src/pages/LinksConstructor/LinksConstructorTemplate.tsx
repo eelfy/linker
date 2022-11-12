9b1eac650de2b4faaa7904e21e7e8bcba225fdbd
eelfy <shaggy.za.5727@gmail.com>
2022-11-12 20:01:00 +0300

    start using UserName component

--- frontend/src/pages/LinksConstructor/LinksConstructorTemplate.tsx
+++ frontend/src/pages/LinksConstructor/LinksConstructorTemplate.tsx
@@ -1,17 +1,12 @@
 import { FC } from 'react';
+import UserName from '../../components/UserName';
 import LinksTable from '../../components/LinksTable';
 import { ConstructorWrapper } from './LinksConstructorStyled';
 import { LinksConstructorTemplateProps } from './LinksConstructorTypes';
 
 const LinksConstructorTemplate: FC<LinksConstructorTemplateProps> = () => (
   <ConstructorWrapper>
-    <h1 style={{
-      color: 'var(--color-table-header-font)',
-    }}
-    >
-      user name
-
-    </h1>
+    <UserName />
     <LinksTable />
   </ConstructorWrapper>
 );
